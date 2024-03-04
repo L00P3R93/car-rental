78dfa22adeadaef915f6a77af03fe7f345701cc4
Vincent Kioko <vincentkioko51@gmail.com>
2024-03-05 01:01:58 +0300

    Cars Filter Options

---
+++ components/Home/CarsFilterOption.tsx
@@ -0,0 +1,25 @@
+const CarsFilterOption = () => {
+    return (
+        <div className="mt-10 flex items-center justify-between">
+            <div>
+                <h2 className="text-[30px] font-bold">Cars Catalog</h2>
+                <h2>Explore Available Cars</h2>
+            </div>
+            <div className="flex gap-5">
+                <select className="select select-bordered w-full max-w-xs">
+                    <option disabled selected>Price</option>
+                    <option>Min to Max</option>
+                    <option>Max to Min</option>
+                </select>
+                <select className="select select-bordered w-full md:block max-w-xs hidden">
+                    <option disabled selected>Manufacturer</option>
+                    <option>Toyota</option>
+                    <option>Mitsubishi</option>
+                    <option>BMW</option>
+                </select>
+            </div>
+        </div>
+    );
+};
+
+export default CarsFilterOption;
